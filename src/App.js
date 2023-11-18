6e502b592f45ed532e5e0e69325d32ede0bbadb8
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-18 16:40:20 +0530

    Project init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DialogBox from "./components/DialogBox";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <DialogBox />
+    </div>
+  );
+}
