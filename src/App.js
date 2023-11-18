449ca6b373150c35fd3682c93fa97da9a230c77e
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-18 17:24:12 +0530

    Created a input and output interface
    - test input with RE
    - Enter key is submit
    - some styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import DialogBox from "./components/DialogBox";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-black text-white">
       <DialogBox />
     </div>
   );
--- src/components/DialogBox.js
+++ src/components/DialogBox.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function DialogBox() {
-  return <div>DialogBox</div>;
+  const [inputWord, setinputWord] = useState("");
+  const [outputWord, setOutputWord] = useState("");
+  const handleChange = (e) => {
+    setinputWord(e.target.value);
+  };
+  const handleSubmit = () => {
+    const isAlphabetic = /^[A-Za-z]+$/u.test(inputWord);
+    if (!isAlphabetic) {
+      alert(
+        "Enter only a single word which contains english alphabet letters only"
+      );
+      setinputWord("");
+    } else setOutputWord(inputWord);
+  };
+  return (
+    <div>
+      <div>
+        <h2 className=" py-2">Hi! Please enter a word to jumble:</h2>
+        <input
+          type="text"
+          className="border rounded-md p-2 ring-1 border-blue-700 bg-slate-800"
+          value={inputWord}
+          onChange={handleChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleSubmit();
+          }}
+        />
+        <button
+          className=" bg-blue-700 p-2 ml-2 rounded-md"
+          onClick={handleSubmit}
+        >
+          Go!
+        </button>
+      </div>
+      <div className=" text-center">
+        <h2 className="py-2 m-2 border-dashed border-2 border-blue-700 hover:border-solid">
+          {outputWord}
+        </h2>
+      </div>
+    </div>
+  );
 }
