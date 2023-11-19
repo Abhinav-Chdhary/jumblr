a39a4aec783a76a3a9ebc024f258f8ef30ee5a50
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-19 19:40:05 +0530

    Added Title bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import DialogBox from "./components/DialogBox";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateGame from "./pages/CreateGame";
+import TitleBar from "./components/TitleBar";
 
 export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
+      <TitleBar />
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
