7f2f3f5705c255b4420098fa90a2563a5b17cd1e
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-23 18:02:15 +0530

    Update CreateGame.js

--- src/pages/CreateGame.js
+++ src/pages/CreateGame.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import DialogBox from "../components/DialogBox";
 
 export default function CreateGame() {
   return (
     <div>
+      <h2 className=" py-2">Hi! Please enter 5 word to jumble:</h2>
       <DialogBox />
+      <DialogBox />
+      <DialogBox />
+      <DialogBox />
+      <DialogBox />
+      <Link to="/run">
+        <button className="bg-blue-700 text-3xl font-bold py-2 px-5 rounded-md">
+          Done ⮚
+        </button>
+      </Link>
     </div>
   );
 }
