341372824dd3e38992d105d2e883177f6c438825
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-27 14:17:47 +0530

    Update CheckerBox.js
    - getting input from user
    - some styling

--- src/components/CheckerBox.js
+++ src/components/CheckerBox.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CheckerBox({ question, answer }) {
+  const [userAnswer, setUserAnswer] = useState("");
   return (
-    <div>
-      <div>
+    <div className="flex items-center m-2">
+      <h2 className="border-solid border-2 border-blue-700 p-2 rounded-md">
+        {question}
+      </h2>
+      <div className="ml-4">
         <input
           type="text"
           className="border rounded-md p-2 ring-1 border-blue-700 bg-slate-800"
-          ceholder="original word"
+          placeholder="What can it be?"
+          value={userAnswer}
+          onChange={(e) => {
+            setUserAnswer(e.target.value);
+          }}
         />
-        <button className=" bg-white p-2 ml-2 rounded-md text-md hover:bg-gray-300 active:bg-gray-400">
+        <button className="bg-white p-2 ml-2 rounded-md text-md hover:bg-gray-300 active:bg-gray-400">
           🔍
         </button>
-      </div>
-      <div className=" text-center">
-        <h2 className="py-2 m-2 border-dashed border-2 border-blue-700 hover:border-solid">
-          <div className="opacity-30">"Answer"</div>
-        </h2>
       </div>
     </div>
   );
