8c49ac1dbd0add268f2b8135de84e93f5b842a39
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-27 14:08:13 +0530

    Update CreateGame.js
    Refactoring

--- src/pages/CreateGame.js
+++ src/pages/CreateGame.js
@@ -3,61 +3,44 @@ import { useNavigate } from "react-router-dom";
 import DialogBox from "../components/DialogBox";
 import { useWordContext } from "../components/WordContext";
 
+const initialArray = ["", "", "", "", ""];
+
 export default function CreateGame() {
   const navigator = useNavigate();
-  const { addOriginalWords } = useWordContext();
-  const { addJumbledWords } = useWordContext();
-  const [myWords, setMyWords] = useState(["", "", "", "", ""]);
-  const [myJumbledWords, setJumbledWords] = useState(["", "", "", "", ""]);
-  const handleOriginalWordUpdate = (word, index) => {
-    setMyWords((prevWords) => {
+  const { addOriginalWords, addJumbledWords } = useWordContext();
+  const [myWords, setMyWords] = useState([...initialArray]);
+  const [myJumbledWords, setMyJumbledWords] = useState([...initialArray]);
+
+  const handleWordUpdate = (word, index, setter) => {
+    setter((prevWords) => {
       const newWords = [...prevWords];
       newWords[index] = word;
       return newWords;
     });
   };
-  const handleJumbleWordUpdate = (word, index) => {
-    setJumbledWords((prevWords) => {
-      const newWords = [...prevWords];
-      newWords[index] = word;
-      return newWords;
-    });
-  };
+
   const handleGameCreated = (e) => {
-    //console.log(myWords, myJumbledWords);
     e.preventDefault();
     addOriginalWords(myWords);
     addJumbledWords(myJumbledWords);
     navigator("/run");
   };
+
   return (
     <div>
-      <h2 className=" py-2">Hi! Please enter 5 word to jumble:</h2>
-      <DialogBox
-        onOriginalWordUpdate={handleOriginalWordUpdate}
-        onJumbleWordUpdate={handleJumbleWordUpdate}
-        index={0}
-      />
-      <DialogBox
-        onOriginalWordUpdate={handleOriginalWordUpdate}
-        onJumbleWordUpdate={handleJumbleWordUpdate}
-        index={1}
-      />
-      <DialogBox
-        onOriginalWordUpdate={handleOriginalWordUpdate}
-        onJumbleWordUpdate={handleJumbleWordUpdate}
-        index={2}
-      />
-      <DialogBox
-        onOriginalWordUpdate={handleOriginalWordUpdate}
-        onJumbleWordUpdate={handleJumbleWordUpdate}
-        index={3}
-      />
-      <DialogBox
-        onOriginalWordUpdate={handleOriginalWordUpdate}
-        onJumbleWordUpdate={handleJumbleWordUpdate}
-        index={4}
-      />
+      <h2 className="py-2">Hi! Please enter 5 words to jumble:</h2>
+      {[0, 1, 2, 3, 4].map((index) => (
+        <DialogBox
+          key={index}
+          onOriginalWordUpdate={(word) =>
+            handleWordUpdate(word, index, setMyWords)
+          }
+          onJumbleWordUpdate={(word) =>
+            handleWordUpdate(word, index, setMyJumbledWords)
+          }
+          index={index}
+        />
+      ))}
       <button
         className="bg-blue-700 text-3xl font-bold py-2 px-5 rounded-md my-4 hover:bg-blue-800 active:bg-blue-600"
         onClick={handleGameCreated}
