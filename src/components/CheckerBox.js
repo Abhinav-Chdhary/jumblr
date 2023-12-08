e310474a1583282b8f34bb871d4329d6193aa7d0
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-12-08 10:33:16 +0530

    Update CheckerBox.js
    Fix: Resubmission increases score

--- src/components/CheckerBox.js
+++ src/components/CheckerBox.js
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 export default function CheckerBox({ question, answer, onUpdate }) {
   const [userAnswer, setUserAnswer] = useState("");
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+
   const handleChange = (e) => {
-    setUserAnswer(e.target.value);
+    if (!isAnswerCorrect) {
+      setUserAnswer(e.target.value);
+    }
   };
+
+  const handleCheck = useCallback(() => {
+    if (!isAnswerCorrect) {
+      const correct = userAnswer === answer;
+      onUpdate(correct ? 10 : 0);
+      setIsAnswerCorrect(correct);
+    }
+  }, [answer, isAnswerCorrect, onUpdate, userAnswer]);
+
   useEffect(() => {
     handleCheck();
-  }, [userAnswer]);
+  }, [handleCheck]);
 
-  const handleCheck = () => {
-    onUpdate(userAnswer === answer ? 10 : 0);
-  };
   return (
     <div className="flex items-center m-2">
       <h2 className="border-solid border-2 border-blue-700 p-2 rounded-md w-32 text-center">
@@ -25,9 +35,10 @@ export default function CheckerBox({ question, answer, onUpdate }) {
           placeholder="What can it be?"
           value={userAnswer}
           onChange={handleChange}
+          disabled={isAnswerCorrect} // Disable the input if the answer is correct
         />
         <span className="bg-white p-2 ml-2 rounded-md text-md">
-          {userAnswer === answer ? <>✅</> : <>🔍</>}
+          {isAnswerCorrect ? <>✅</> : <>🔍</>}
         </span>
       </div>
     </div>
