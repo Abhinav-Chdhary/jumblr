61b829a6b8bbd2a04909038f67f5897f125121ae
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-12-08 10:02:27 +0530

    Update AlertModal.js
    Fix: Overlay hiding button

--- src/components/AlertModal.js
+++ src/components/AlertModal.js
@@ -2,15 +2,11 @@ import React from "react";
 
 export default function AlertModal({ isOpen, onClose }) {
   return (
-    <div
-      className={`${
-        isOpen ? "fixed" : "hidden"
-      } inset-0 h-full w-full z-50 overflow-hidden flex justify-center items-center animated fadeIn`}
-    >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+    <div className="fixed inset-0 h-full w-full z-50 overflow-hidden flex justify-center items-center animated fadeIn bg-black">
+      <div className="absolute inset-0 bg-black opacity-50 pointer-events-none"></div>
 
-      <div className="bg-white w-1/3 p-6 rounded-md shadow-md">
-        <div className="mb-4 text-center text-xl font-bold  bg-black">
+      <div className=" bg-red-500 w-1/3 p-6 rounded-md shadow-md relative">
+        <div className="mb-4 text-center text-xl font-bold  ">
           You must input 5 words
         </div>
 
