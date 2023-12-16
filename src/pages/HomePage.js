b140d8d4887aa3fc418f8a87d804e3fcdb5ab9ef
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-12-16 10:43:12 +0530

    Public links for images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,10 +4,10 @@ import RulesCarousel from "../components/RulesCarousel";
 
 export default function HomePage() {
   const slides = [
-    <img src="/1.png" alt="Slide 1" />,
-    <img src="/2.png" alt="Slide 2" />,
-    <img src="/3.png" alt="Slide 3" />,
-    <img src="/4.png" alt="Slide 4" />,
+    <img src={process.env.PUBLIC_URL + "/1.png"} alt="Slide 1" />,
+    <img src={process.env.PUBLIC_URL + "/2.png"} alt="Slide 2" />,
+    <img src={process.env.PUBLIC_URL + "/3.png"} alt="Slide 3" />,
+    <img src={process.env.PUBLIC_URL + "/4.png"} alt="Slide 4" />,
   ];
   return (
     <div className="flex flex-col items-center">
