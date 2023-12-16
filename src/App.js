7528da3c849a48502f54a4c13863bdf28c6af7f7
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-12-16 10:48:29 +0530

    Routing update for publishing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import CreateGame from "./pages/CreateGame";
 import TitleBar from "./components/TitleBar";
 import Rungame from "./pages/Rungame";
 import { WordProvider } from "./components/WordContext";
+const basename = process.env.NODE_ENV === "development" ? "/" : "/jumblr";
 
 export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
       <WordProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
           <TitleBar />
           <Routes>
             <Route exact path="/" element={<HomePage />} />
